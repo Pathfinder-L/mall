62d20d3bb9b2a02eb93c7ff431c9315f246ea42c
pathfinder_l <2185945875@qq.com>
2024-01-10 10:25:23 +0800

    搭建项目结构

---
+++ mall-coupon/src/test/java/org/example/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
